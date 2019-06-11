71333e426853cd5489ed7ec6413aaac06f10ddd2
thanhnhannguyen308 <34601934+thanhnhannguyen308@users.noreply.github.com>
2019-06-11 23:50:48 +0700

    commit migration and repository

---
+++ NguyenNhanShop.Data/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using NguyenNhanShop.Data.Infrastructure;
+using NguyenNhanShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NguyenNhanShop.Data.Repository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        
+    }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+
+        
+    }
+}
