9c2ef30d991d674f35138c17eeb486f7e68a8fb7
thanhnhannguyen308 <34601934+thanhnhannguyen308@users.noreply.github.com>
2019-06-16 23:18:50 +0700

    add more repository for orther tables

---
+++ NguyenNhanShop.Data/Repository/OrderDetailRepository.cs
@@ -0,0 +1,21 @@
+﻿using NguyenNhanShop.Data.Infrastructure;
+using NguyenNhanShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NguyenNhanShop.Data.Repository
+{
+    public interface IOrderDetailRepository : IRepository<OrderDetail>
+    {
+    }
+
+    public class OrderDetailRepository : RepositoryBase<OrderDetail>, IOrderDetailRepository
+    {
+        public OrderDetailRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ NguyenNhanShop.Data/Repository/TagRepository.cs
@@ -0,0 +1,21 @@
+﻿using NguyenNhanShop.Data.Infrastructure;
+using NguyenNhanShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NguyenNhanShop.Data.Repository
+{
+    public interface ITagRepository : IRepository<Tag>
+    {
+    }
+
+    public class TagRepository : RepositoryBase<Tag>, ITagRepository
+    {
+        public TagRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
